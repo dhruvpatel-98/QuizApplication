0e31ab99aef17bbb7ca6e6275e31a4acf19041f6
josephsteenhuisen <joseph.steenhuisen@uconn.edu>
2022-10-30 20:20:26 -0400

    ver_1

--- src/Components/Questions.java
+++ src/Components/Questions.java
@@ -2,11 +2,38 @@ package Components;
 
 public class Questions {
     private String question;
+    private String question2;
+
+    private String question3;
+    private String question4;
+
+    private String question5;
+
+    private String question6;
+
     private String answer;
 
-    public Questions(String q, String a) {
+    private String answer2;
+    private String answer3;
+    private String answer4;
+
+    private String answer5;
+
+    private String answer6;
+
+    public Questions(String q, String a,String q2, String b, String q3, String c, String q4, String d, String q5, String e, String q6, String f) {
         question = q;
+        question2 = q2;
+        question3 = q3;
+        question4 = q4;
+        question5 = q5;
+        question6 = q6;
         answer = a;
+        answer2 = b;
+        answer3 = c;
+        answer4 = d;
+        answer5 = e;
+        answer6 = f;
     }
 
     public String getQuestion() {
@@ -17,6 +44,45 @@ public class Questions {
         question = q;
     }
 
+    public String getQuestion2() {
+        return question2;
+    }
+
+    public void setQuestion2(String q2) {
+        question2 = q2;
+    }
+
+    public String getQuestion3() {
+        return question3;
+    }
+
+    public void setQuestion3(String q3) {
+        question3 = q3;
+    }
+    public String getQuestion4() {
+        return question4;
+    }
+
+    public void setQuestion4(String q4) {
+        question4 = q4;
+    }
+
+    public String getQuestion5() {
+        return question5;
+    }
+
+    public void setQuestion5(String q5) {
+        question5 = q5;
+    }
+    public String getQuestion6() {
+        return question6;
+    }
+
+    public void setQuestion6(String q6) {
+        question6 = q6;
+    }
+
+
     public String getAnswer() {
         return answer;
     }
@@ -24,6 +90,43 @@ public class Questions {
     public void setAnswer(String a) {
         answer = a;
     }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String b) {
+        answer2 = b;
+    }
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String c) {
+        answer3 = c;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String d) {
+        answer4 = d;
+    }
+
+    public String getAnswer5() {
+        return answer5;
+    }
+
+    public void setAnswer5(String e) {
+        answer5 = e;
+    }
+
+    public String getAnswer6() {
+        return answer6;
+    }
+
+    public void setAnswer6(String f) { answer6 = f; }
 
     public String toString() {
         return "[]";
--- src/Components/restServer.java
+++ src/Components/restServer.java
@@ -1,19 +1,19 @@
 package Components;
 
 
-import org.restlet.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 
     public class restServer extends ServerResource {
 
         //private static int count = 0;
         private static String json_string;
+        private static String json_string2;
 
         public static void main(String[] args) throws Exception {
 
@@ -21,9 +21,10 @@ import com.google.gson.GsonBuilder;
             builder.setPrettyPrinting();
             Gson gson = builder.create();
 
-            Questions one_q = new Questions("How many members of the US Congress?", "435");
+            Questions one_q = new Questions("How many members of the US Congress?", "435", "For the last 8 years of his life, Galileo was under house arrest for espousing this man's theory", "Copernicus", "No. 2: 1912 Olympian; football star at Carlisle Indian School; 6 MLB seasons with the Reds, Giants & Braves", "Jim Thorpe", "The city of Yuma in this state has a record average of 4,055 hours of sunshine each year", "Arizona", "In 1963, live on The Art Linkletter Show, this company served its billionth burger", "McDonald's", "Signer of the Dec. of Indep., framer of the Constitution of Mass., second President of the United Stzxates","John Adams");
 
             json_string = gson.toJson(one_q);
+
 
 
             // Create the HTTP server and listen on port 8182
@@ -36,5 +37,6 @@ import com.google.gson.GsonBuilder;
             // count++;
             return  json_string;  // "Question " + Integer.valueOf(count).toString() + ": " +
         }
+
 
     }
